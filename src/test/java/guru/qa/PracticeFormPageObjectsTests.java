a5062d88133e32d729f78d84794f12fcd492ca94
ElenaPchelkina <56049246+ElenaPchelkina@users.noreply.github.com>
2021-09-11 14:10:28 +0300

    Added allure attachments

--- src/test/java/guru/qa/PracticeFormPageObjectsTests.java
+++ src/test/java/guru/qa/PracticeFormPageObjectsTests.java
@@ -1,11 +1,15 @@
 package guru.qa;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.github.javafaker.Faker;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationPage;
+import helpers.Attach;
 
 
 public class PracticeFormPageObjectsTests {
@@ -32,11 +36,23 @@ public class PracticeFormPageObjectsTests {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.startMaximized = true;
 
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
 
         Configuration.browserCapabilities = capabilities;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
     }
 
     @Test
